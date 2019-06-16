a9305b9425f4769b69dce540080014befd511db4
gzboy <boy.gengzhi@gmail.com>
2019-06-15 23:26:59 -0500

    Added savetoBigTable function

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"cloud.google.com/go/storage"
-	"context"
+	"context"	
 	"encoding/json"
 	"fmt"
 	"github.com/auth0/go-jwt-middleware"
@@ -10,6 +10,7 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/pborman/uuid"
 	elastic "gopkg.in/olivere/elastic.v3"
+	"cloud.google.com/go/bigtable"
 	"io"
 	"log"
 	"net/http"
@@ -36,6 +37,8 @@ const (
 	TYPE        = "post"
 	DISTANCE    = "200km"
 	ES_URL      = "http://35.225.62.93:9200"
+	PROJECT_ID = "around-243514"
+	BT_INSTANCE = "around-post"
 )
 
 var mySigningKey = []byte("secret")
@@ -152,6 +155,32 @@ func handlerPost(w http.ResponseWriter, r *http.Request) {
 
 	// Save to ES.
 	saveToES(p, id)
+
+	//Save to BigTable
+	saveToBigTable(p, id)
+}
+
+func saveToBigTable(p *Post, id string) {
+	ctx := context.Background()
+	// you must update project name here
+	bt_client, err := bigtable.NewClient(ctx, PROJECT_ID, BT_INSTANCE) 
+	if err != nil {
+		panic(err)
+		return 
+	}
+	tbl := bt_client.Open("post") 
+	mut := bigtable.NewMutation() //one row 
+	t := bigtable.Now()
+	mut.Set("post", "user", t, []byte(p.User))
+	mut.Set("post", "message", t, []byte(p.Message))
+	mut.Set("location", "lat", t, []byte(strconv.FormatFloat(p.Location.Lat, 'f', -1, 64)))
+	mut.Set("location", "lon", t, []byte(strconv.FormatFloat(p.Location.Lon, 'f', -1, 64)))
+	err = tbl.Apply(ctx, id, mut) 
+	if err != nil {
+		panic(err)
+		return 
+	}
+	fmt.Printf("Post is saved to BigTable: %s\n", p.Message)
 }
 
 func saveToGCS(ctx context.Context, r io.Reader, bucketName, name string) (*storage.ObjectHandle, *storage.ObjectAttrs, error) {
